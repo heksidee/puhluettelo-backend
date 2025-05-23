04191e92cc9a7b6f35e545f11aaf581be07c6cc2
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-05-23 13:37:30 +0300

    poistettu cors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const cors = require("cors");
+
 const morgan = require("morgan");
 
 const app = express()
 
-app.use(cors());
+
 app.use(express.json());
 
 morgan.token("body", (request) => JSON.stringify(request.body));
