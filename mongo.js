6959c93eb3adbf6460b90810ba7fdd9c8314f5e5
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-05-26 15:54:29 +0300

    asennettu ja luotu mongo

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.a5qfl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const phoneBookSchema = new mongoose.Schema({
+  name: String,
+  number: Boolean,
+})
+
+const Phonebook = mongoose.model("Person", phoneBookSchema)
+
+const phonebook = new Phonebook({
+    name: name,
+    number: number,
+})
+
+if (process.argv.length === 4) {
+    phonebook.save().then(result => {
+       console.log(`added ${name} number ${number} to phonebook`) 
+       mongoose.connection.close()
+    })
+}
+
+if (process.argv.length === 3) {
+    Phonebook.find({}).then(result => {
+        console.log("phonebook:")
+        result.forEach(person => {
+            console.log(name, number)
+        })
+        mongoose.connection.close()
+    })
+}
